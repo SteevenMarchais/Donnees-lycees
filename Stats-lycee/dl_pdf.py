adf7649e1a80dcecafb393f7afe5856235a3dcf4
SteevenMarchais <steeven.marchais@gmail.com>
2022-01-15 17:02:32 +0100

    code_amelioré

--- Stats-lycee/dl_pdf.py
+++ Stats-lycee/dl_pdf.py
@@ -34,11 +34,13 @@ def get_pdfs_Poitier():
     shutil.move("./" + file1, "../data/" + file1)
     pdf1 = open("../data/" + file1, "rb")
     pdfReader = PyPDF2.PdfFileReader(pdf1) 
-    print(pdfReader.numPages)
-    pageObj = pdfReader.getPage(1)  
-    test = pageObj.extractText()
-    print(test)
-    pdf1.close()  
+    page=(pdfReader.numPages)
+    for i in range(0, page):
+        pageObj = pdfReader.getPage(i)  
+        text = pageObj.extractText()
+        print(text)
+    pdf1.close()
+      
 
     
 
@@ -59,27 +61,18 @@ def get_pdfs_Bordeaux():
             else:
                 links.append(base.scheme + "://" + base.netloc + current_link)
 
-def get_pdfs_Limoges():
-    my_url = "https://www.ac-bordeaux.fr/article/second-degre-121965"
-    links = []
-    html = urllib.request.urlopen(my_url).read()
-    html_page = bs4.BeautifulSoup(html, features="lxml") 
-    og_url = html_page.find("meta",  property = "og:url")
-    base = urllib.request.urlparse(my_url)
-    for link in html_page.find_all('a'):
-        current_link = link.get('href')
-        if current_link.endswith('download'):
-            if og_url:
-                print("currentLink",current_link)
-                links.append("https://www.ac-bordeaux.fr" + current_link)
-            else:
-                links.append(base.scheme + "://" + base.netloc + current_link)
 
    
-    
     file2 = wget.download(links[0])
     shutil.move("./" + file2, "../data/" + file2)
-
+    pdf1 = open("../data/" + file2, "rb")
+    pdfReader = PyPDF2.PdfFileReader(pdf1) 
+    page=(pdfReader.numPages)
+    for i in range(0, page):
+        pageObj = pdfReader.getPage(i)  
+        text = pageObj.extractText()
+        print(text)
+    pdf1.close()
 
     
 
