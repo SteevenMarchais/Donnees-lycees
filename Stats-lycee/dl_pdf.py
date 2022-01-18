0579b92e1b0f2150a209f6da81b46669c3aae3db
SteevenMarchais <steeven.marchais@gmail.com>
2022-01-18 17:18:24 +0100

    docstring

--- Stats-lycee/dl_pdf.py
+++ Stats-lycee/dl_pdf.py
@@ -13,84 +13,98 @@ import PyPDF2
 
 
 def get_pdfs_Poitier():
+    
+    """
+    Télécharge et affiche le texte nécessaire du pdf
+    
+    Entree
+    ----------
+    my_url:str
+    url du site web sur lequel se trouve le pdf
+    
+    Return
+    ---------
+    text:str
+    texte extrait du pdf
+    """
     my_url = "https://www.ac-poitiers.fr/minihome/publications-statistiques-122378"
     links = []
-    html = urllib.request.urlopen(my_url).read()
+    html = urllib.request.urlopen(my_url).read() #ouvre le site web
     html_page = bs4.BeautifulSoup(html, features="lxml") 
-    og_url = html_page.find("meta",  property = "og:url")
+    og_url = html_page.find("meta",  property = "og:url") #cherche les liens dans la page html
     base = urllib.request.urlparse(my_url)
-    for link in html_page.find_all('a'):
-        current_link = link.get('href')
-        if current_link.endswith('download'):
+    for link in html_page.find_all('a'): #cherche les balises a
+        current_link = link.get('href')#récupere le lien
+        if current_link.endswith('download'):#regarde s'il y a download à la fin du lien
             if og_url:
                 print("currentLink",current_link)
-                links.append("https://www.ac-poitiers.fr" + current_link)
+                links.append("https://www.ac-poitiers.fr" + current_link)#complete l'url
             else:
                 links.append(base.scheme + "://" + base.netloc + current_link)
 
    
-    file1 = wget.download(links[3])
-    shutil.move("./" + file1, "../data/" + file1)
+    file1 = wget.download(links[3])#télécharge le 4ème pdf
+    shutil.move("./" + file1, "../data/" + file1)#envoie le pdf dans le fichier data
+    """
+    lire et extraire les données du pdf
+    """
     pdf1 = open("../data/" + file1, "rb")
     pdfReader = PyPDF2.PdfFileReader(pdf1) 
     print(pdfReader.numPages)
     pageObj = pdfReader.getPage(0)  
-    test = pageObj.extractText()
-    c = test.index("Terminales")
+    text = pageObj.extractText()
+    c = text.index("Terminales")
     for i in range(c, c+38):
-        print(test[i])
+        print(text[i])
     pdf1.close() 
     
 
     
 def get_pdfs_Bordeaux():
+    """
+    Télécharge et affiche le texte nécessaire du pdf
+    
+    Entree
+    ----------
+    my_url:str
+    url du site web sur lequel se trouve le pdf
+    
+    Return
+    ---------
+    text:str
+    texte extrait du pdf
+    """
     my_url = "https://www.ac-bordeaux.fr/article/second-degre-121965"
     links = []
-    html = urllib.request.urlopen(my_url).read()
+    html = urllib.request.urlopen(my_url).read()#ouvre le site web
     html_page = bs4.BeautifulSoup(html, features="lxml") 
-    og_url = html_page.find("meta",  property = "og:url")
+    og_url = html_page.find("meta",  property = "og:url")#cherche les liens dans la page html
     base = urllib.request.urlparse(my_url)
-    for link in html_page.find_all('a'):
-        current_link = link.get('href')
-        if current_link.endswith('download'):
+    for link in html_page.find_all('a'):#cherche les balises a
+        current_link = link.get('href')#cherche les balises a
+        if current_link.endswith('download'):#regarde s'il y a download à la fin du lien
             if og_url:
                 print("currentLink",current_link)
-                links.append("https://www.ac-bordeaux.fr" + current_link)
+                links.append("https://www.ac-bordeaux.fr" + current_link)#complete l'url
             else:
                 links.append(base.scheme + "://" + base.netloc + current_link)
 
-    file1 = wget.download(links[0])
-    shutil.move("./" + file1, "../data/" + file1)
+    file1 = wget.download(links[0])#télécharge le 1er pdf
+    shutil.move("./" + file1, "../data/" + file1)#envoie le pdf dans le fichier data
+    """
+    lire et extraire les données du pdf
+    """
     pdf1 = open("../data/" + file1, "rb")
     pdfReader = PyPDF2.PdfFileReader(pdf1) 
     print(pdfReader.numPages)
     pageObj = pdfReader.getPage(5)  
-    test = pageObj.extractText()
-    print(test)
+    text = pageObj.extractText()
+    print(text)
     pdf1.close() 
 
-
-# def get_pdfs_Limoges():
-#     my_url = "https://www.ac-bordeaux.fr/article/second-degre-121965"
-#     links = []
-#     html = urllib.request.urlopen(my_url).read()
-#     html_page = bs4.BeautifulSoup(html, features="lxml") 
-#     og_url = html_page.find("meta",  property = "og:url")
-#     base = urllib.request.urlparse(my_url)
-#     for link in html_page.find_all('a'):
-#         current_link = link.get('href')
-#         if current_link.endswith('download'):
-#             if og_url:
-#                 print("currentLink",current_link)
-#                 links.append("https://www.ac-bordeaux.fr" + current_link)
-#             else:
-#                 links.append(base.scheme + "://" + base.netloc + current_link)
-
-   
+# def main():
     
-#     file2 = wget.download(links[0])
-#     shutil.move("./" + file2, "../data/" + file2)
-
-
+# if __name__ == "__main__":
+#     main()
     
 
